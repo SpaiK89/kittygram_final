d27bc920c78ce1d3edd17cc5de5de81e50483aa4
Spaik <spaik111@gmail.com>
2023-05-19 00:56:15 +0300

    fixed media

--- backend/kittygram_backend/urls.py
+++ backend/kittygram_backend/urls.py
@@ -5,7 +5,6 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
 
-
 router = routers.DefaultRouter()
 router.register(r'cats', CatViewSet)
 router.register(r'achievements', AchievementViewSet)
@@ -13,8 +12,8 @@ router.register(r'achievements', AchievementViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
-    path('api/', include('djoser.urls')),  
-    path('api/', include('djoser.urls.authtoken')), 
+    path('api/', include('djoser.urls')),
+    path('api/', include('djoser.urls.authtoken')),
 ]
 
 if settings.DEBUG:
