017be4818a5db33e4dfda1894068261c91d24054
Spaik <spaik111@gmail.com>
2023-05-18 19:10:37 +0300

    first version

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -98,13 +98,13 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'collected_static'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = '/var/www/kittygram/media'#os.path.join(BASE_DIR, '/var/www/kittygram/media')
+MEDIA_ROOT = '/var/www/kittygram/media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated', 
+        'rest_framework.permissions.IsAuthenticated',
     ],
 
     'DEFAULT_AUTHENTICATION_CLASSES': [
