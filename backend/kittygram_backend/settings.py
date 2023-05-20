edd1551f900e27e3c13368e6b74cb84c14fa6041
Igor Bogomolov <spaik111@gmail.com>
2023-05-20 13:50:12 +0300

    fixed project

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key_for_github_tests')
 
 DEBUG = (os.getenv('DEBUG', 'False') == 'True')
 
